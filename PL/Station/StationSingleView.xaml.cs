dee206e9fe38aae91dace89d9e3365aa2f31a3bc
tamar edri <tedri@g.jct.ac.il>
2021-12-27 15:42:24 +0200

    top bar

--- PL/Station/StationSingleView.xaml.cs
+++ PL/Station/StationSingleView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace PL.Station
+namespace PL
 {
     /// <summary>
     /// Interaction logic for StationSingleView.xaml
